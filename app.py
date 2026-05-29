e91cf96175e59c93ac7b639fd6e00893ac7a3bae
Himan <himan.bhat10@gmail.com>
2026-05-30 01:46:54 +0530

    Fix duplicate send route

--- app.py
+++ app.py
@@ -10,12 +10,8 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-
-# YOUR EMAIL
 app.config['MAIL_USERNAME'] = 'himan.bhat10@gmail.com'
-
-# APP PASSWORD
-app.config['MAIL_PASSWORD'] = 'cszk rzhd luck dfjw'
+app.config['MAIL_PASSWORD'] = 'YOUR_NEW_APP_PASSWORD'
 
 mail = Mail(app)
 
@@ -31,57 +27,37 @@ def home():
 # CONTACT FORM
 # ==========================
 
-
-
-# ==========================
-
-if __name__ == "__main__":
-    app.run
-
 @app.route("/send", methods=["POST"])
 def send():
 
     try:
-
         name = request.form.get("name")
         email = request.form.get("email")
         message = request.form.get("message")
 
-        print("FORM DATA RECEIVED")
-        print(name)
-        print(email)
-        print(message)
-
         msg = Message(
-
             subject=f"New Portfolio Inquiry from {name}",
-
             sender=app.config['MAIL_USERNAME'],
-
             recipients=['himan.bhat10@gmail.com']
-
         )
 
         msg.body = f"""
-
 Name: {name}
 
 Email: {email}
 
 Message:
 {message}
-
 """
 
         mail.send(msg)
 
-        print("EMAIL SENT SUCCESSFULLY")
-
         return redirect("/")
 
     except Exception as e:
-
-        print("ERROR OCCURRED")
-        print(e)
-
         return str(e)
+
+# ==========================
+
+if __name__ == "__main__":
+    app.run()
